641ae205a8c9778791c4bed73e1fe5c893b3ec65
unknown <muhammedbinramli@gmail.com>
2025-04-07 16:47:39 +0530

    working on product page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./client/App.jsx";
-import { createBrowserRouter, RouterProvider,Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import Home from "./client/pages/Home.jsx";
 import Wishlist from "./client/pages/Wishlist.jsx";
+import Productpage from "./client/pages/productpage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {index:true, element:<Navigate to="home"/> },
+      { index: true, element: <Navigate to="home" /> },
       { path: "home", element: <Home /> },
       { path: "wishlist", element: <Wishlist /> },
+      { path: "product", element: <Productpage /> },
     ],
   },
 ]);
