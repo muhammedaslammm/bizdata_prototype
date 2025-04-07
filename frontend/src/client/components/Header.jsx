a2277a3875ab321a3e7806b19ba650d903e6c147
aslammm <muhammedbinramli@gmail.com>
2025-04-07 12:03:06 +0530

    project deal added

--- frontend/src/client/components/Header.jsx
+++ frontend/src/client/components/Header.jsx
@@ -2,16 +2,16 @@ import categories from "../../data/categories";
 
 const Header = () => {
   return (
-    <div className="bg-white">
+    <header className="bg-white">
       <header className=" flex justify-between items-center w-[90%] mx-auto py-[.8rem]">
-        <div className="logo text-gray-800 font-medium text-[2.3rem] capitalize">
+        <div className="logo text-gray-800 font-medium text-[2.4rem] capitalize">
           prototype
         </div>
         <search>
           <input type="search" name="" id="" />
         </search>
         <nav>
-          <ul className="options flex gap-[2.8rem] text-[1.6rem]">
+          <ul className="options flex gap-[2.8rem] text-[1.7rem]">
             <li className="capitalize cursor-pointer">cart</li>
             <li className="capitalize cursor-pointer">whishlist</li>
             <li className="capitalize cursor-pointer">profile</li>
@@ -23,14 +23,14 @@ const Header = () => {
         <ul className="flex text-[1.3rem] w-[90%] mx-auto  text-white">
           {categories.map(function (category) {
             return (
-              <li className="uppercase text-[1.5rem] py-[.55rem] px-4 cursor-pointer hover:bg-sky-700">
+              <li className="uppercase text-[1.6rem] py-[.57rem] px-4 cursor-pointer hover:bg-sky-700">
                 {category.name}
               </li>
             );
           })}
         </ul>
       </nav>
-    </div>
+    </header>
   );
 };
 
--- frontend/src/client/pages/Home.jsx
+++ frontend/src/client/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import Deal from "../components/Deal";
 
 const Home = () => {
   return (
     <div className="home my-[1.5rem]">
       <Banner />
+      <Deal />
     </div>
   );
 };
