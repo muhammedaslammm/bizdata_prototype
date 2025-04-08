15108455869090540a7f21f65a08a1f139583b3a
aslammm <muhammedbinramli@gmail.com>
2025-04-08 15:05:21 +0530

    category data updated

--- frontend/src/data/categories.js
+++ frontend/src/data/categories.js
@@ -11,8 +11,46 @@ const categories = [
           { name: "ultra HD" },
         ],
       },
-      { name: "shop tv by screen" },
-      { name: "large home appliances" },
+      {
+        name: "shop tv by screen",
+        child_categories: [
+          { name: "below 32 inch" },
+          { name: "32 inch" },
+          { name: "39 inch" },
+        ],
+      },
+      {
+        name: "large home appliances",
+        child_categories: [
+          { name: "air conditioners" },
+          { name: "window acs" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "mobiles",
+    child_categories: [
+      {
+        name: "smartphones & tablets",
+        child_categories: [
+          { name: "i kall" },
+          { name: "mi" },
+          { name: "realme" },
+          { name: "samsung" },
+          { name: "vivo" },
+          { name: "oppo" },
+        ],
+      },
+      {
+        name: "feature phones",
+        child_categories: [
+          { name: "micromax" },
+          { name: "i kall" },
+          { name: "nokia" },
+          { name: "heemax" },
+        ],
+      },
     ],
   },
   { name: "women" },
