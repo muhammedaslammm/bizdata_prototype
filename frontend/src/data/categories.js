4df01b3765ec8ee313186689867dcd1c5097682c
aslammm <muhammedbinramli@gmail.com>
2025-04-08 11:36:43 +0530

    working with the categories

--- frontend/src/data/categories.js
+++ frontend/src/data/categories.js
@@ -1,6 +1,21 @@
 const categories = [
+  {
+    name: "appliances",
+    sub_categories: [
+      {
+        name: "tv & dth",
+        child_categories: [
+          { name: "television" },
+          { name: "full HD" },
+          { name: "HD ready" },
+          { name: "ultra HD" },
+        ],
+      },
+      { name: "shop tv by screen" },
+      { name: "large home appliances" },
+    ],
+  },
   { name: "women" },
-  { name: "men" },
   { name: "home & kitchen" },
   { name: "appliances" },
   { name: "sports & more" },
