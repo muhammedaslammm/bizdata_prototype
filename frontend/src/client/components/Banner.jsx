376b2ccda7c91287366e452c3d093cd77fc58a3d
Muhammed Aslam <133602147+muhammedaslammm@users.noreply.github.com>
2025-04-15 12:46:42 +0530

    banner and header updated (#18)

--- frontend/src/client/components/Banner.jsx
+++ frontend/src/client/components/Banner.jsx
@@ -1,25 +1,55 @@
-import banners from '../../data/banners.js'
-import bannerlinks from '../../data/bannerlinks.js';
+import banners from "../../data/banners.js";
+import bannerlinks from "../../data/bannerlinks.js";
+import { useEffect, useState } from "react";
+
 const Banner = () => {
-    return (
-        <div className="banner w-[90%] mx-auto flex gap-[2rem]">
-            <div className="first w-[80%] h-[45rem]">
-                <img src={banners[0].image} alt="" className='w-full h-full object-cover' />
-            </div>
-            {bannerlinks.length ? <div className='w-[20%] bg-white flex flex-col border border-neutral-300'>
-                {bannerlinks.map((link) => {
-                    if (link.active) {
-                        return (
-                            <div className='flex flex-col grow-[1] items-center py-[1.5rem] border-b border-gray-200 last:border-0 '>
-                                <div className="image w-[9.5rem] h-[9.5rem]"><img src={link.image} alt="" className='w-full h-full' /></div>
-                                <p className='text-[1.3rem] font-medium capitalize'>{link.title}</p>
-                            </div>
-                        )
-                    }
-                })}
-            </div> : <></>}
+  const [banner, setBanner] = useState(null);
+  useEffect(() => {
+    const homeBanner = banners.find((ban) => ban.name === "home-banner");
+    setBanner(homeBanner);
+  }, []);
+  return banner ? (
+    <div className="banner w-[90%] mx-auto flex gap-[2rem] z-0">
+      <div className="first w-[80%] h-[45rem] relative">
+        <img
+          src={banner.image}
+          alt=""
+          className="w-full h-full object-cover rounded-[.5rem] z-0"
+        />
+        <div className="heads  absolute bottom-[9rem] left-[2.8rem] leading-[3.5rem]">
+          <h1 className="text-gray-200 text-[3.5rem] font-medium capitalize">
+            {banner.heading}
+          </h1>
+          <p className="text-gray-400 text-[2rem]">{banner.banner_note}</p>
+          <button className="text-[1.35rem] capitalize px-[1rem] text-gray-300 border border-gray-500 rounded-[.4rem] mt-[1rem] cursor-pointer transition-all duration-300 hover:text-white hover:bg-gray-950 hover:border-gray-400">
+            shop latest arrivals
+          </button>
         </div>
-    )
-}
+      </div>
+      {bannerlinks.length ? (
+        <div className="w-[20%] bg-white flex flex-col border border-neutral-300 rounded-[.5rem]">
+          {bannerlinks.map((link) => {
+            if (link.active) {
+              return (
+                <div className="flex flex-col grow-[1] items-center py-[1.5rem] border-b border-gray-200 last:border-0 ">
+                  <div className="image w-[9.5rem] h-[9.5rem]">
+                    <img src={link.image} alt="" className="w-full h-full" />
+                  </div>
+                  <p className="text-[1.3rem] font-medium capitalize">
+                    {link.title}
+                  </p>
+                </div>
+              );
+            }
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  ) : (
+    <></>
+  );
+};
 
 export default Banner;
--- frontend/src/data/banners.js
+++ frontend/src/data/banners.js
@@ -2,7 +2,7 @@ const banners = [
   {
     name: "home-banner",
     heading: "The all new are out here",
-    paragraph: "Grab now for the best deal",
+    banner_note: "Grab now for the best deal",
     image:
       "https://img.freepik.com/free-photo/laptop-with-glowing-screen-table-dark-top-view-copy-space_169016-51607.jpg?semt=ais_hybrid",
   },
