b3ae3ebd870748c6c01d9251dc29361067149723
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-04-19 11:32:27 +0530

    banner data updated

--- frontend/src/data/banners.js
+++ frontend/src/data/banners.js
@@ -6,6 +6,21 @@ const banners = [
     image:
       "https://img.freepik.com/free-photo/laptop-with-glowing-screen-table-dark-top-view-copy-space_169016-51607.jpg?semt=ais_hybrid",
   },
+  {
+    name: "projector",
+    image:
+      "https://t3.ftcdn.net/jpg/08/76/40/84/360_F_876408456_dnUJyNXyPNLxqVbSc53aH4c6d2FYS6eq.jpg",
+  },
+  {
+    name: "laptop",
+    iamge:
+      "https://t3.ftcdn.net/jpg/08/76/40/84/360_F_876408456_dnUJyNXyPNLxqVbSc53aH4c6d2FYS6eq.jpg",
+  },
+  {
+    name: "monitor",
+    image:
+      "https://t3.ftcdn.net/jpg/08/76/40/84/360_F_876408456_dnUJyNXyPNLxqVbSc53aH4c6d2FYS6eq.jpg",
+  },
 ];
 
 export default banners;
