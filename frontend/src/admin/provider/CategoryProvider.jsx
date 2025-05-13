840d27638900d07b2fb15ec60c87139e2d08b2d4
aslammm <muhammedbinramli@gmail.com>
2025-05-13 14:12:34 +0530

    env configured

--- frontend/src/admin/provider/CategoryProvider.jsx
+++ frontend/src/admin/provider/CategoryProvider.jsx
@@ -6,10 +6,13 @@ const CategoryProvider = ({ children }) => {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await fetch("http://localhost:4000/api/categories", {
-          method: "GET",
-          credentials: "include",
-        });
+        const response = await fetch(
+          `${process.env.REACT_APP_BACKEND_URL}/admin/categories`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
         const data = await response.json();
         if (!response.ok) throw new Error(data.message);
         else {
